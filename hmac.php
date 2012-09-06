84cfa17ebf7af55710673d80f962f45b470681e0
maxiPago! <dev@maxipago.com>
2012-09-06 12:18:59 -0300

    Updated constant name

--- hmac.php
+++ hmac.php
@@ -4,11 +4,11 @@ $sig_id = ""; // Random 4-digit number
 $amount = ""; // Transaction amount
 $item_id = ""; // SKU or Item ID
 
-define("secretKey", "secret_key"); // Secret key assigned by maxiPago!
+define("hashKey", "your-secret-key-here"); // Secret key assigned by maxiPago!
 define("SEP", "*"); // Separator. DO NOT CHANGE
 
 $toHash = $gmt_ts.SEP.$sig_id.SEP.$amount.SEP.$item_id;
-$hmac = hash_hmac("MD5", $toHash, secretKey);
+$hmac = hash_hmac("MD5", $toHash, hashKey);
 
 echo "Signature: " . $hmac;
 ?>
