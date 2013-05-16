ecdd64b0da1d7ea00f0bc0bc0cf7e7d0393a5e77
Tom Canabarro <tcanabarro@gmail.com>
2013-05-16 18:21:05 -0300

    Created PHP Hmac class

--- hmac.php
+++ hmac.php
@@ -1,14 +1,15 @@
 <?php
-$gmt_ts = ""; // Timestamp in epoch format
-$sig_id = ""; // Random 4-digit number
-$amount = ""; // Transaction amount
-$item_id = ""; // SKU or Item ID
-
-define("hashKey", "your-secret-key-here"); // Secret key assigned by maxiPago!
-define("SEP", "*"); // Separator. DO NOT CHANGE
-
-$toHash = $gmt_ts.SEP.$sig_id.SEP.$amount.SEP.$item_id;
-$hmac = hash_hmac("MD5", $toHash, hashKey);
-
-echo "Signature: " . $hmac;
+class hmachHash {
+	
+	public function getRequestSignature($hp_sig_timestamp,$hp_sig_id,$hp_amount,$hp_sig_itemid,$secretKey) {
+		$toHash = $hp_sig_timestamp."*".$hp_sig_id."*".$hp_amount."*".$hp_sig_itemid;
+		return hash_hmac("MD5", $toHash, $secretKey);
+	}
+	
+	public function getResponseSignature($hp_refnum,$hp_amount,$hp_responsemsg,$secretKey) {
+		$toHash = $hp_refnum."*".$hp_amount."*".$hp_responsemsg;
+		return hash_hmac("MD5", $toHash, $secretKey);
+	}
+	
+}
 ?>
