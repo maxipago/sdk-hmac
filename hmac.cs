b1eb71d72a76342455455429713a1934568f5a67
Tom Canabarro <tcanabarro@gmail.com>
2013-05-16 18:21:15 -0300

    Added .NET Hmac lib

---
+++ hmac.cs
@@ -0,0 +1,4 @@
+##################################################
+## Please see the following library on Codeplex ##
+##         http://hmacmd5.codeplex.com/         ##
+##################################################
